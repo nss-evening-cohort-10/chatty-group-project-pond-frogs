bb66bfc9b52c4205cc40c08f2782ded85762cf24
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-10-18 17:04:13 -0500

    added some styling to message bubbles

--- src/javascripts/components/addMessage/addMessage.js
+++ src/javascripts/components/addMessage/addMessage.js
@@ -1,10 +1,14 @@
 import $ from 'jquery';
 import utilities from '../../helpers/utlities';
+import './addMessage.scss';
 
 let domString = '';
 
 const printMessage = () => {
+  domString += '<div id="message-bubble">';
+  domString += '<div class="arrow bottom right"></div>';
   domString += $('#message-input').val();
+  domString += '</div>';
   utilities.printToDom('message-flow', domString);
 };
 
