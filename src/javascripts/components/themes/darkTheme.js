94132000eeaf5ba0139f21110ae4196bcc62af51
Jacob <wittenberg.jacob@gmail.com>
2019-10-19 10:25:41 -0500

    just a small merge/commit for getting message styling all looking the same without roling out a whole new function

---
+++ src/javascripts/components/themes/darkTheme.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+
+import './darkTheme.scss';
+
+const darkTheme = () => {
+  if ($('darkThemeCheck:checked')) {
+    // change colors on page
+  }
+};
+
+export default { darkTheme };
