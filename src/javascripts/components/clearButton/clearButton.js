19bc64ab308b940da7732ac2cf2b328b139f885e
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-10-19 14:19:40 -0500

    disabled clear button

--- src/javascripts/components/clearButton/clearButton.js
+++ src/javascripts/components/clearButton/clearButton.js
@@ -2,9 +2,14 @@ import $ from 'jquery';
 
 import utilities from '../../helpers/utlities';
 
+const disableButton = () => {
+  $('.clear').attr('disabled', true);
+};
+
 const clearButton = () => {
   $('body').on('click', '.clear', (e) => {
     e.preventDefault();
+    disableButton();
     utilities.printToDom('message-field', '');
   });
 };
--- src/javascripts/helpers/data/messages.js
+++ src/javascripts/helpers/data/messages.js
@@ -1,23 +1,23 @@
 const seedData = [
   {
-    message: 'Why was the JavaScript developer sad?</p><p>Because they didn’t Node how to Express themself',
-    timestamp: '',
+    message: 'Why was the JavaScript developer sad? Because they didn’t Node how to Express themself',
+    timestamp: 'July 4th, 1776 12:00 AM',
   },
   {
-    message: 'Why did the react class component feel relieved?</p><p>Because it was now off the hook.',
-    timestamp: '',
+    message: 'Why did the react class component feel relieved? <<So>Because it was now off the hook.',
+    timestamp: 'Oct. 19th, 1973 6:15 AM',
   },
   {
-    message: 'How do you comfort a JavaScript bug?</p><So>You console it.',
-    timestamp: '',
+    message: 'How do you comfort a JavaScript bug? <So>You console it.',
+    timestamp: 'Jan. 1st, 1983 6:19 PM',
   },
   {
     message: 'And the bartender says, "Success, but you\'re not ready!" So a JavaScript function walks into a bar.',
-    timestamp: '',
+    timestamp: 'Nov. 18th, 2012 2:52 PM',
   },
   {
-    message: '<p>Thanks for using ClapBack! Please type your message in the input field, change the color theme, or clear the page!</p>',
-    timestamp: '',
+    message: 'Thanks for using ClapBack! Please type your message in the input field, change the color theme, or clear the page! Oh and you never respond back to      me!',
+    timestamp: 'Oct. 19th, 12:11 PM',
   },
 
 ];
