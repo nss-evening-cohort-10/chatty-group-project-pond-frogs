2e98fb56381460419c7e8ff6aed48af20c222c3b
Jacob <wittenberg.jacob@gmail.com>
2019-10-21 18:24:51 -0500

    savepoint

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,16 +1,18 @@
 import 'bootstrap';
 import displayMessages from './components/displayMessages/displayMessages';
-
-import '../styles/main.scss';
 import navbar from './components/navbar/navbar';
 import add from './components/addMessage/addMessage';
 import clear from './components/clearButton/clearButton';
+import defaultThemes from './components/defaultThemes/defaultThemes';
+
+import '../styles/main.scss';
 
 const init = () => {
-  displayMessages.printMessageCards();
-  navbar.printNavbar();
   add.addMessage();
   clear.clearButton();
+  defaultThemes.changeTheme();
+  displayMessages.printMessageCards();
+  navbar.printNavbar();
 };
 
 init();
