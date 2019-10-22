7fc93823ed97b844ac8b313191415cf183c29c91
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-10-22 13:08:27 -0500

    started restting input field

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,16 +1,12 @@
 import 'bootstrap';
 import displayMessages from './components/displayMessages/displayMessages';
-<<<<<<< HEAD
 import limit from './helpers/data/messages';
 import '../styles/main.scss';
-=======
->>>>>>> master
 import navbar from './components/navbar/navbar';
 import add from './components/addMessage/addMessage';
 import clear from './components/clearButton/clearButton';
 import defaultThemes from './components/defaultThemes/defaultThemes';
 
-import '../styles/main.scss';
 
 const init = () => {
   navbar.printNavbar();
